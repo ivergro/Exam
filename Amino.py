7be635502673dba867919578b300fe02fcdd2e39
Iver Grønlien <iver@gronlien.no>
2023-05-02 11:45:01 +0200

    Updated Amino. NN now a list. Help functions made

--- Amino.py
+++ Amino.py
@@ -4,6 +4,7 @@ import numpy as np
 #Kan gjøre sånn at når en amino blir en annens NN, får den andre også den sin NN
 #Må kanskje ikke ha NN avhening av pos, kan også bare ha en liste
 #NN må skille på chain og bare nearest neigbhour
+#Må kunne fjærne nærmeste aminoer
 class Amino:
     """
     Amino class, or monomer class. 
@@ -29,7 +30,8 @@ class Amino:
     """
     def __init__(self, type : int, pos : tuple, index : int, dim : int = 2) -> None:
         self._type = type
-        self._NN = np.zeros(2*dim)
+        #self._NN = np.zeros(2*dim)
+        self._NN = []
         self._pos = pos
         self._index = index
 
@@ -50,42 +52,50 @@ class Amino:
     #-----------Setters------------
     def set_pos(self, pos) -> None:
         self._pos = pos
+    
+    def set_NN(self, amino):
+        (self._NN).append(amino)
+
+    def remove_NN(self, amino):
+        (self._NN).remove(amino)
+        return amino
 
     #-0-
     #3.1
     #-2-     . marks amino position, 3D gives 4 and 5 upwards then downwards
-    def set_NN(self, amino, relative_pos : tuple):
-        if len(relative_pos) == 2:
-            match relative_pos:
-                case (0,1):
-                    self._NN[0] = amino
-                case (1,0):
-                    self._NN[1] = amino
-                case (0,-1):
-                    self._NN[2] = amino
-                case (-1,0):
-                    self._NN[3] = amino
+    #Blir nok tungvindt, må bytte pos om en flytter på seg
+    # def set_NN(self, amino, relative_pos : tuple):
+    #     if len(relative_pos) == 2:
+    #         match relative_pos:
+    #             case (0,1):
+    #                 self._NN[0] = amino
+    #             case (1,0):
+    #                 self._NN[1] = amino
+    #             case (0,-1):
+    #                 self._NN[2] = amino
+    #             case (-1,0):
+    #                 self._NN[3] = amino
         
-        elif len(relative_pos) == 3:
-             match relative_pos:
-                case (0,1,0):
-                    self._NN[0] = amino
-                case (1,0,0):
-                    self._NN[1] = amino
-                case (0,-1,0):
-                    self._NN[2] = amino
-                case (-1,0,0):
-                    self._NN[3] = amino
-                case (0,0,1):
-                    self._NN[4] = amino
-                case (0,0,-1):
-                    self._NN[5] = amino
+    #     elif len(relative_pos) == 3:
+    #          match relative_pos:
+    #             case (0,1,0):
+    #                 self._NN[0] = amino
+    #             case (1,0,0):
+    #                 self._NN[1] = amino
+    #             case (0,-1,0):
+    #                 self._NN[2] = amino
+    #             case (-1,0,0):
+    #                 self._NN[3] = amino
+    #             case (0,0,1):
+    #                 self._NN[4] = amino
+    #             case (0,0,-1):
+    #                 self._NN[5] = amino
 
 
     #-----------Help functions----------
     def is_nearest_neighbour(self, amino) -> bool:
         #Checks if they are covalently bonded
-        if abs(self.get_index() - amino.get_index()) is not 1:
+        if (abs(self.get_index() - amino.get_index()) is not 1):
             #Substracting each x-y-z component to check distance vector
             difference = tuple(map(lambda i, j: i - j, self.get_pos, amino.get_pos()))
 
@@ -94,6 +104,24 @@ class Amino:
                 return True
         return False
     
-    #def check_NN(self, amino) -> bool:
+    def find_nearest_neigbours(self, chain) -> None:
+        for amino in chain:
+            #Checks if theyre neigbhours, and if theyre already in the list
+            if (self.is_nearest_neighbour(amino)) and (amino not in self.get_NN()):
+                #Sets both as each others NN
+                self.set_NN(amino)
+                amino.set_NN(self)
+
+    #Could make a tempchain, pop NN values, and loop over it the second time to minimize runtime
+    def update_NN(self, chain) -> None:
+        #Removing old ones first
+        for old_NN in self._NN:
+            if not self.is_nearest_neighbour(old_NN):
+                self.remove_NN(old_NN)
+        
+        #Updating
+        for amino in chain:
+            if self.is_nearest_neighbour(amino):
+                self.set_NN(amino)
 
     
--- tasks.py
+++ tasks.py
@@ -5,5 +5,12 @@ def task_21_1():
     N = 15 #number of monomers
     chain = np.zeros(15)
     for i in range(N):
-        chain[i] = Amino(i+1, (i,0), dim=2)
+        chain[i] = Amino(i+1, (i,0), i, dim=2)
+    
+    #Finding nearest neigbhours
+    for i in range(N):
+        chain[i].find_nearest_neighbour(chain)
+    
+def task_21_2():
+    pass
     
